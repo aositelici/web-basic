049bb64f79fc9e573217d6762b2391b5eb80bffa
liya <liya013@live.com>
2015-08-13 16:52:38 +0800

    add home-view-model

---
+++ view-models/home-view-model.js
@@ -0,0 +1,61 @@
+var TopicHelper = require('../helper/topic-helper');
+var Fill = require('../model/fill');
+var Choice = require('../model/choice');
+var MultiChoice = require('../model/multi-choice');
+var TrueFalse = require('../model/true-false');
+var ShortAnswer = require('../model/short-answer');
+function HomeViewModel (topics,inputs) {
+  this.topics = topics;
+  this.inputs = inputs;
+}
+HomeViewModel.prototype.getFillTopics = function() {
+  var topics = [];
+  this.topics.forEach(function(topic) {
+    if(topic instanceof Fill){
+      topics.push(topic);
+    }
+  })
+  return topics;
+}
+
+HomeViewModel.prototype.getChoiceTopics = function() {
+  var topics = [];
+  this.topics.forEach(function(topic) {
+    if(topic instanceof Choice){
+      topics.push(topic);
+    }
+  })
+  return topics;
+}
+
+HomeViewModel.prototype.getMultiChoiceTopics = function() {
+  var topics = [];
+  this.topics.forEach(function(topic) {
+    if(topic instanceof MultiChoice){
+      topics.push(topic);
+    }
+  })
+  return topics;
+}
+
+HomeViewModel.prototype.getTrueFalseTopics= function() {
+  var topics = [];
+  this.topics.forEach(function(topic) {
+    if(topic instanceof TrueFalse){
+      topics.push(topic);
+    }
+  })
+  return topics;
+}
+
+HomeViewModel.prototype.getShortAnswerTopics= function() {
+  var topics = [];
+  this.topics.forEach(function(topic) {
+    if(topic instanceof ShortAnswer){
+      topics.push(topic);
+    }
+  })
+  return topics;
+}
+
+module.exports = HomeViewModel;
