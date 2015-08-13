848a1ffd1e44db47045d11dbc03e02a6bde3a59e
liya <liya013@live.com>
2015-08-13 16:03:51 +0800

    add helper

---
+++ helper/topic-helper.js
@@ -0,0 +1,18 @@
+var TopicFactory = require('../model/topic-factory');
+
+function TopicHelper () {
+
+}
+
+TopicHelper.prototype.getTopics = function(questions) {
+
+  var topics = [];
+  var topicFactory = new TopicFactory();
+
+  for(var type in questions) {
+
+    topics.push(topicFactory.getTopic(questions[type]));
+  }
+
+  return topics;
+}
