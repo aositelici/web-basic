2bdda169d814c9e19543d590faed2c960a6cf0fb
liya <liya013@live.com>
2015-08-14 22:02:53 +0800

    modify because new helpers

--- controller/home-controller.js
+++ controller/home-controller.js
@@ -1,7 +1,8 @@
 var  HomeViewModel = require('../view-models/home-view-model');
 var TopicHelper = require('../helper/topic-helper');
 var data = require('../data.json');
-
+var MarkerHelper = require('../helper/marker-helper');
+var InfoHelper = require('../helper/info-helper');
 function HomeController() {
 
 }
@@ -19,10 +20,18 @@ HomeController.prototype.submit = function(req,res) {
 
   var topicHelper = new TopicHelper();
   var topics = topicHelper.getTopics(data.topics);
-  var userInput = req.body;
-
-  var homeViewModel = new HomeViewModel(topics,userInput);
+  var userInputs = req.body;
+  
+  var homeViewModel = new HomeViewModel(topics,userInputs);
   homeViewModel.setUserInput();
+  var marker = new MarkerHelper();
+  homeViewModel.totalScore = marker.getTotalScore(userInputs,topics);
+
+  var infoHelper = new InfoHelper();
+  var info = infoHelper.getUserInfo(userInputs);
+  homeViewModel.Name = info.name;
+  homeViewModel.Class = info.class;
+  homeViewModel.Number = info.number;
 
   res.render('index',homeViewModel);
 
--- view-models/home-view-model.js
+++ view-models/home-view-model.js
@@ -7,16 +7,21 @@ var ShortAnswer = require('../model/short-answer');
 function HomeViewModel(topics, inputs) {
   this.topics = topics;
   this.inputs = inputs;
+  this.totalScore = '';
+  this.Name ='';
+  this.Class ='';
+  this.Number = '';
 }
+
 HomeViewModel.prototype.getFillTopics = function () {
   var topics = [];
   this.topics.forEach(function (topic) {
     if (topic instanceof Fill) {
       topics.push(topic);
     }
-  })
+  });
   return topics;
-}
+};
 
 HomeViewModel.prototype.getChoiceTopics = function () {
   var topics = [];
@@ -24,9 +29,9 @@ HomeViewModel.prototype.getChoiceTopics = function () {
     if (topic instanceof Choice) {
       topics.push(topic);
     }
-  })
+  });
   return topics;
-}
+};
 
 HomeViewModel.prototype.getMultiChoiceTopics = function () {
   var topics = [];
@@ -34,9 +39,9 @@ HomeViewModel.prototype.getMultiChoiceTopics = function () {
     if (topic instanceof MultiChoice) {
       topics.push(topic);
     }
-  })
+  });
   return topics;
-}
+};
 
 HomeViewModel.prototype.getTrueFalseTopics = function () {
   var topics = [];
@@ -44,9 +49,9 @@ HomeViewModel.prototype.getTrueFalseTopics = function () {
     if (topic instanceof TrueFalse) {
       topics.push(topic);
     }
-  })
+  });
   return topics;
-}
+};
 
 HomeViewModel.prototype.getShortAnswerTopics = function () {
   var topics = [];
@@ -54,9 +59,9 @@ HomeViewModel.prototype.getShortAnswerTopics = function () {
     if (topic instanceof ShortAnswer) {
       topics.push(topic);
     }
-  })
+  });
   return topics;
-}
+};
 
 HomeViewModel.prototype.setUserInput = function () {
 
@@ -70,43 +75,6 @@ HomeViewModel.prototype.setUserInput = function () {
     }
     topic.setInput(userInput);
   })
+};
 
-
-}
-HomeViewModel.prototype.getScores = function () {
-  if (!this.inputs) {
-
-    return '';
-  }
-  else {
-
-    var scores = 0;
-    this.topics.forEach(function (topic) {
-      scores += topic.mark();
-    })
-
-    return scores;
-  }
-}
-
-HomeViewModel.prototype.getUserInfo = function () {
-  if (this.inputs) {
-    return {
-      name: this.inputs.name,
-      class: this.inputs.class,
-      number: this.inputs.number,
-      score: this.getScores()
-    }
-  }
-  else {
-    return {
-      name: '',
-      class: '',
-      number: '',
-      score: ''
-    }
-  }
-
-
-}
 module.exports = HomeViewModel;
