2b9363cc0e6cda23364b8345cdc31406451f1d2c
liya <liya013@live.com>
2015-08-14 22:00:48 +0800

    new class to mark and set info

---
+++ helper/info-helper.js
@@ -0,0 +1,22 @@
+function InfoHelper() {
+
+}
+
+InfoHelper.prototype.getUserInfo = function (inputs) {
+  if (inputs) {
+    return {
+      name: inputs.name,
+      class: inputs.class,
+      number: inputs.number
+    }
+  }
+  else {
+    return {
+      name: '',
+      class: '',
+      number: ''
+    }
+  }
+};
+
+module.exports = InfoHelper;
---
+++ helper/marker-helper.js
@@ -0,0 +1,21 @@
+function MarkerHelper() {
+
+}
+
+MarkerHelper.prototype.getTotalScore = function(inputs,topics) {
+  if (!inputs) {
+
+    return '';
+  }
+  else {
+
+    var score = 0;
+    topics.forEach(function (topic) {
+      score += topic.mark();
+    });
+
+    return score;
+  }
+}
+
+module.exports = MarkerHelper;
