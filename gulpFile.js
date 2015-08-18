7e48827a1588b0cad126dcc01c63eed9e25f9e8f
liya <liya_013@live.com>
2015-08-18 23:47:40 +0800

    finish automaticrestart

--- gulpFile.js
+++ gulpFile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
 
+var paths = ['./view-models/*', './views/*','./views/partial/*',
+  './seeds/*','./routes/*','./public/javascript/*','./model/*',
+  './helper/*','./controller/*','./*'];
 
 gulp.task('init',function(){
-  exec('mysql -u root -p123456 <./seeds/test.sql');
+  exec('mysql -u root -p123456 < ./seeds/test.sql');
 });
 
 gulp.task('start',function(){
@@ -11,5 +14,6 @@ gulp.task('start',function(){
 });
 
 gulp.task('default', function() {
-
+  exec('node app.js');
+  gulp.watch(paths, ['start']);
 });
