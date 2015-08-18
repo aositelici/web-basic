5bff4d4bfffeeb9b157ce22d8aa0091d23310d1e
liya <liya_013@live.com>
2015-08-18 21:36:12 +0800

    connect database and get data

---
+++ helper/paperData-helper.js
@@ -0,0 +1,53 @@
+'use strict';
+
+function PaperDataHelper() {
+
+}
+
+PaperDataHelper.prototype.getPaperData = function(callback) {
+
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+    user     : 'root',
+    password : '123456',
+    database : 'TWWebBasic'
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected');
+  });
+
+  var topics = [];
+
+  var select = 'SELECT topics.*,ops.item,ops.title FROM topics LEFT JOIN options ops ON topics.topicId = ops.itemId';
+   connection.query(select,function(err,rows) {
+    topics = rows;
+
+    var option = [];
+    var formatTopics = [];
+
+    for(var i = 0; i < topics.length; i++) {
+       if(option[topics[i].topicId]) {
+         option[topics[i].topicId].push({item:topics[i].item, describe:topics[i].title});
+       }
+       else {
+         option[topics[i].topicId] = [];
+         option[topics[i].topicId].push({item:topics[i].item, describe:topics[i].title});
+       }
+      formatTopics[topics[i].topicId-1] ={type:topics[i].type, name:topics[i].name, question:topics[i].questions,
+      answer:topics[i].answer.split(' '), score:topics[i].score, options:option[topics[i].topicId]};
+    }
+    callback(formatTopics);
+  });
+
+
+
+};
+
+module.exports = PaperDataHelper;
+
+
